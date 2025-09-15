f36f029010c28138aee979e8ec5b7fadc00851c3
Paolo D'Alberto <paolo.dalberto@gmail.com>
2025-09-14 17:05:11 -0700

    cheers

--- Code/q.py
+++ Code/q.py
@@ -6,11 +6,43 @@ from qiskit.primitives import StatevectorSampler as Sampler
 
 from qiskit.visualization import plot_histogram
 
+from qiskit.circuit.library import UnitaryGate
+
+import numpy
+phi = 2*numpy.pi/3
+
+
+# Gemini prompt 
+# an example of unitary gate that split the probability 1/4 and 3/4
+
+# An example of a unitary gate that splits a probability into a 1/4 and a 3/4 chance is a rotation gate.
+# A rotation about the y-axis, \R_{y}(\theta ), can transform an initial state |0> into a superposition of |0> and |1>.
+# For an initial state of |0>, the resulting state is given by the first column of the rotation matrix:
+#
+# R_{y}(\theta)|0> =\begin{matrix}\cos (\theta /2)\ \sin (\theta /2)\end{matrix}.
+#
+# To achieve probabilities of 1/4 and 3/4, the squared magnitudes of
+# the amplitudes must be \(|\cos (\theta /2)|^{2}=1/4\) and \(|\sin (\theta /2)|^{2}=3/4\).
+
+# This gives a rotation angle of \(\theta \) where \(\cos (\theta /2)=\pm 1/2\) and \(\sin (\theta /2)=\pm \sqrt{3}/2\). A common
+# solution is \(\theta /2=\pi /3\), which means \(\theta =2\pi /3\).
+#
+# The unitary matrix for this gate 
+
+matrix = [
+    [ numpy.cos(phi),  -numpy.sin(phi)],
+    [ numpy.sin(phi),  numpy.cos(phi)]
+]
+g_p = UnitaryGate(matrix)
+ 
+
+
 # Create a quantum circuit with 2 qubits
 bell_circuit = QuantumCircuit(2)
 
 # Apply a Hadamard gate to the first qubit to create a superposition
 bell_circuit.h(0)
+bell_circuit.append(g_p, [1])
 
 # Apply a CNOT gate to entangle the qubits
 bell_circuit.cx(0, 1)
