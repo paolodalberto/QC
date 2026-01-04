7f2d67a67b0b487fc3b5061fee4dc0ef231cf37f
Paolo D'Alberto <paolo.dalberto@gmail.com>
2026-01-04 10:47:47 -0800

    .

--- Code/Circuit4G/H2.cpp
+++ Code/Circuit4G/H2.cpp
@@ -117,10 +117,10 @@ void set_temp(std::vector<int> gpus,
     ZC *d_state=0, *d_temp=0; 
 
     printf(" GPU %d alloc %zu \n", gpu, state_bytes);
-    CHECK_HIP(hipSetDevice(gpu));
-    CHECK_HIP(hipMalloc(&d_state, state_bytes));
     
     if (permutation[gpu] !=gpu ) {
+      CHECK_HIP(hipSetDevice(gpu));
+      CHECK_HIP(hipMalloc(&d_state, state_bytes));
       printf("\t GPU %d temp %zu \n", gpu, state_bytes);
       CHECK_HIP(hipMalloc(&d_temp, temp_bytes));
     } //else temp_bytes=0;
@@ -253,16 +253,42 @@ void run_shuffle_test(int BITS, std::vector<int> permutation) {
   }
 }
 
+
+/*
+
+========================================= ROCm System Management Interface =========================================
+=================================================== Concise Info ===================================================
+Device  [Model : Revision]    Temp    Power     Partitions      SCLK    MCLK     Fan     Perf  PwrCap  VRAM%  GPU%  
+        Name (20 chars)       (Edge)  (Socket)  (Mem, Compute)                                                      
+====================================================================================================================
+0       [Radeon Pro Duo : 0x  30.0°C  11.08W    N/A, N/A        567Mhz  300Mhz   19.22%  auto  98.0W     0%   0%    
+        Ellesmere [Radeon Pr                                                                                        
+1       [Radeon Pro Duo : 0x  29.0°C  20.082W   N/A, N/A        849Mhz  300Mhz   100.0%  auto  98.0W     0%   0%    
+        Ellesmere [Radeon Pr                                                                                        
+2       [0x081e : 0xc1]       32.0°C  19.0W     N/A, N/A        808Mhz  350Mhz   19.61%  auto  250.0W    0%   0%    
+        Vega 20 [Radeon VII]                                                                                        
+3       [0x081e : 0xc1]       33.0°C  19.0W     N/A, N/A        808Mhz  350Mhz   19.61%  auto  250.0W    0%   0%    
+        Vega 20 [Radeon VII]                                                                                        
+4       [0x081e : 0xc1]       33.0°C  24.0W     N/A, N/A        808Mhz  1000Mhz  19.61%  auto  250.0W    3%   0%    
+        Vega 20 [Radeon VII]                                                                                        
+====================================================================================================================
+=============================================== End of ROCm SMI Log ================================================
+
+
+ */
+
+
+
 int main(int argc, char* argv[]) {
   // Method 1: Using atoi (C-style, simpler but less robust)
   int BITS  = (argc>1)? std::atoi(argv[1]):20;
 
   // This is the n+2-n bit for 4 GPU
   // 0 - 2 and 1-3
-  // 0 - 1 and 2-3 is the n+1 - n bit  
-  std::vector<int> permutation{2,3,0,2,
-    4 // the last gpu stays put 
-  };
+  // 0 - 1 and 2-3 is the n+1 - n bit
+
+  // VIDEO --------------------------v
+  std::vector<int> permutation{2,3,0,1,4};
   
   try {
     run_shuffle_test(BITS,permutation);
